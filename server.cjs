ffb25b65df503c9ed629ccd62bb3ad6c4426bbc5
yassmamahamat-tech <yassmamahamat@gmail.com>
2026-05-19 09:51:29 +0100

    fix: use env port

--- server.cjs
+++ server.cjs
@@ -13,7 +13,7 @@ const path    = require('path');
 const fs      = require('fs');
 
 const app  = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
